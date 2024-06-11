3d2fb4795dd491973567535c849f4f10ec6c60d1
dipanakos <95593076+dipanakos@users.noreply.github.com>
2024-06-11 23:24:31 +0300

    Join Tests updated.

--- noda-client/src/test/java/gr/ds/unipi/noda/api/client/CustomJoinTest.java
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/CustomJoinTest.java
@@ -8,6 +8,8 @@ package gr.ds.unipi.noda.api.client;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.spark.sql.SparkSession;
+import org.junit.Assert;
+import org.junit.Test;
 
 import com.google.common.base.Stopwatch;
 
@@ -20,22 +22,52 @@ import gr.ds.unipi.noda.api.mongo.filterOperators.comparisonOperators.MongoDBCom
  */
 public class CustomJoinTest {
 	/**
-	 * @param args
+	 * 
 	 */
-	public static void main (String[] args) {
+//	@Test
+	public void testJoinLarge1 () {
+		NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.MongoDB().Builder("admin01", "pass", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_dynamic")).build();
+		NoSqlDbOperators noSqlDbOperators = noSqlDbSystem.operateOn("nari_dynamic");
+		noSqlDbOperators = noSqlDbOperators.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("sourcemmsi", "227705102"));
+		NoSqlDbSystem noSqlDbSystem1 = NoSqlDbSystem.MongoDB().Builder("admin01", "pass", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_static")).build();
+		NoSqlDbOperators noSqlDbOperators1 = noSqlDbSystem1.operateOn("nari_static");
+		noSqlDbOperators1 = noSqlDbOperators1.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("sourcemmsi", "227705102"));
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		noSqlDbOperators.joinToJSON(noSqlDbOperators1, new EqualJoinOperator("sourcemmsi", "sourcemmsi"));
+		stopwatch.stop();
+		System.out.println(stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
+	}
+	
+//	@Test
+	public void testJoinLarge () {
+		NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.MongoDB().Builder("admin01", "pass", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_dynamic")).build();
+		NoSqlDbOperators noSqlDbOperators = noSqlDbSystem.operateOn("nari_dynamic");
+		noSqlDbOperators = noSqlDbOperators.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("sourcemmsi", "227705102"));
+		NoSqlDbSystem noSqlDbSystem1 = NoSqlDbSystem.MongoDB().Builder("admin01", "pass", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_static")).build();
+		NoSqlDbOperators noSqlDbOperators1 = noSqlDbSystem1.operateOn("nari_static");
+		noSqlDbOperators1 = noSqlDbOperators1.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("sourcemmsi", "227705102"));
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		noSqlDbOperators.joinToJSON(noSqlDbOperators1, new EqualJoinOperator("sourcemmsi", "sourcemmsi"));
+		stopwatch.stop();
+		System.out.println(stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
+	}
+	
+	@Test
+	public void testJoin () {
 		NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.MongoDB().Builder("admin01", "pass", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_dynamic_new")).build();
 		NoSqlDbOperators noSqlDbOperators = noSqlDbSystem.operateOn("nari_dynamic_new");
-		noSqlDbOperators = noSqlDbOperators.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "Belgium"));
+		noSqlDbOperators = noSqlDbOperators.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "-"));
 		NoSqlDbSystem noSqlDbSystem1 = NoSqlDbSystem.MongoDB().Builder("admin01", "pass", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_static_new")).build();
 		NoSqlDbOperators noSqlDbOperators1 = noSqlDbSystem1.operateOn("nari_static_new");
-		noSqlDbOperators1 = noSqlDbOperators1.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "Belgium"));
+		noSqlDbOperators1 = noSqlDbOperators1.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "-"));
 		Stopwatch stopwatch = Stopwatch.createStarted();
-		noSqlDbOperators.joinToJSON(noSqlDbOperators1, new EqualJoinOperator("countryName", "countryName"));
+		String results = noSqlDbOperators.joinToJSON(noSqlDbOperators1, new EqualJoinOperator("countryName", "countryName"));
 		stopwatch.stop();
 		System.out.println(stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
 		noSqlDbSystem.closeConnection();
 		noSqlDbSystem1.closeConnection();
 	}
+
 
 	private static SparkSession createSparkSession (String database, String collection) {
 		return SparkSession.builder().appName("Application Name").master("local").config("spark.mongodb.input.database", database).config("spark.mongodb.input.collection", collection).getOrCreate();
--- noda-client/src/test/java/gr/ds/unipi/noda/api/client/MongoDbJoinTest.java
+++ noda-client/src/test/java/gr/ds/unipi/noda/api/client/MongoDbJoinTest.java
@@ -1,11 +1,17 @@
 package gr.ds.unipi.noda.api.client;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.common.base.Stopwatch;
+
 import gr.ds.unipi.noda.api.core.nosqldb.NoSqlDbOperators;
+import gr.ds.unipi.noda.api.core.operators.joinOperators.EqualJoinOperator;
 import gr.ds.unipi.noda.api.core.operators.joinOperators.MongoDBJoinOperatorFactory;
 import gr.ds.unipi.noda.api.mongo.filterOperators.comparisonOperators.MongoDBComparisonOperatorFactory;
 
@@ -20,11 +26,12 @@ public class MongoDbJoinTest {
 	public void testMethod () {
 		NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.MongoDB().Builder("mongoadmin", "mongoadmin", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_dynamic_new")).build();
 		NoSqlDbOperators noSqlDbOperators = noSqlDbSystem.operateOn("nari_dynamic_new");
-		noSqlDbOperators = noSqlDbOperators.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "Belgium"));
+		noSqlDbOperators = noSqlDbOperators.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "Denmark"));
 		NoSqlDbSystem noSqlDbSystem1 = NoSqlDbSystem.MongoDB().Builder("marinetimeClient", "passw0rd", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_static_new")).build();
 		NoSqlDbOperators noSqlDbOperators1 = noSqlDbSystem1.operateOn("nari_static_new");
-		noSqlDbOperators1 = noSqlDbOperators1.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "Belgium"));
+		noSqlDbOperators1 = noSqlDbOperators1.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("countryName", "Denmark"));
 		//@format:off
+		Stopwatch stopwatch = Stopwatch.createStarted();
 		Dataset<Row> dataset1 = noSqlDbOperators.project("countryName", "sourcemmsi", "shipLength").join(noSqlDbOperators1.project("countryName", "shiptype"), new MongoDBJoinOperatorFactory().newOperatorEq("countryName", "countryName")).toDataframe();
 		long docs = dataset1.count();
 		System.out.println(String.format("Records retrieved size from dataset one is : %s.", docs));
@@ -32,7 +39,36 @@ public class MongoDbJoinTest {
 		long docsReversedJoin = dataset2.count();
 		dataset1.show();
 		dataset2.show();
-		System.out.println(docs == docsReversedJoin ? String.format("Test PASSED with records retrieved size: %s.", docsReversedJoin) : "Test FAILED");
+		Assert.assertEquals(docs, docsReversedJoin);
+		stopwatch.stop();
+		System.out.println(stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
+		System.out.println(String.format("Test PASSED with records retrieved size: %s.", docsReversedJoin + docs));
+		//@format:on
+		noSqlDbSystem.closeConnection();
+		noSqlDbSystem1.closeConnection();
+	}
+
+//	@Test
+	public void testMethodLarge () {
+		NoSqlDbSystem noSqlDbSystem = NoSqlDbSystem.MongoDB().Builder("mongoadmin", "mongoadmin", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_dynamic")).build();
+		NoSqlDbOperators noSqlDbOperators = noSqlDbSystem.operateOn("nari_dynamic");
+		noSqlDbOperators = noSqlDbOperators.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("sourcemmsi", "227705102"));
+		NoSqlDbSystem noSqlDbSystem1 = NoSqlDbSystem.MongoDB().Builder("marinetimeClient", "passw0rd", "marinetime").host("localhost").sparkSession(createSparkSession("marinetime", "nari_static")).build();
+		NoSqlDbOperators noSqlDbOperators1 = noSqlDbSystem1.operateOn("nari_static");
+		noSqlDbOperators1 = noSqlDbOperators1.filter(new MongoDBComparisonOperatorFactory().newOperatorEq("sourcemmsi", "227705102"));
+		//@format:off
+		Dataset<Row> dataset1 = noSqlDbOperators.join(noSqlDbOperators1, new MongoDBJoinOperatorFactory().newOperatorEq("sourcemmsi", "sourcemmsi")).toDataframe();
+		long docs = dataset1.count();
+		System.out.println(String.format("Records retrieved size from dataset one is : %s.", docs));
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		Dataset<Row> dataset2 =noSqlDbOperators1.join(noSqlDbOperators, new MongoDBJoinOperatorFactory().newOperatorEq("sourcemmsi", "sourcemmsi")).toDataframe();
+		long docsReversedJoin = dataset2.count();
+//		dataset1.show();
+//		dataset2.show();
+		Assert.assertEquals(docs, docsReversedJoin);
+		stopwatch.stop();
+		System.out.println(stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
+		System.out.println(String.format("Test PASSED with records retrieved size: %s.", docsReversedJoin + docs));
 		//@format:on
 		noSqlDbSystem.closeConnection();
 		noSqlDbSystem1.closeConnection();
